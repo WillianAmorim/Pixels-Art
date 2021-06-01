c1b2271b38d27ded28e723f36c3237c4459f2940
WillianAmorim <willian.amorim1@outlook.com>
2021-06-01 20:20:23 -0300

    feito desafio 6

---
+++ script.js
@@ -0,0 +1,5 @@
+let color0 = document.getElementsByClassName("color")[0].style.backgroundColor="black"
+let color1 = document.getElementsByClassName("color")[1].style.backgroundColor="yellow"
+let color2 = document.getElementsByClassName("color")[2].style.backgroundColor="blue"
+let color3 = document.getElementsByClassName("color")[3].style.backgroundColor="purple"
+
