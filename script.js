e2ad522e11d8cf9f0ed16e1e85c1418e6cb323bb
WillianAmorim <willian.amorim1@outlook.com>
2021-06-01 23:18:50 -0300

    reparos no linter

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let color0 = document.getElementsByClassName("color")[0].style.backgroundColor="black";
-let color1 = document.getElementsByClassName("color")[1].style.backgroundColor="yellow";
-let color2 = document.getElementsByClassName("color")[2].style.backgroundColor="blue";
-let color3 = document.getElementsByClassName("color")[3].style.backgroundColor="purple";
+let color0 = document.getElementsByClassName("color")[0]
+color0.style.backgroundColor = "black";
+
+let color1 = document.getElementsByClassName("color")[1]
+color1.style.backgroundColor = "yellow";
+
+let color2 = document.getElementsByClassName("color")[2]
+color2.style.backgroundColor = "blue";
+
+let color3 = document.getElementsByClassName("color")[3]
+color3.style.backgroundColor = "purple";
 
 document.get
